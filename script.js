d688a5952092d485eadc019177905d6de80e6a7c
Creator-lab-sj <segunjoshuaadeogun@gmail.com>
2024-09-04 13:50:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+// Example function to log a message (you can add more functionality as needed)
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Mummy Tee website is loaded and ready.");
+});
+
+// Add more JavaScript functionality as needed
